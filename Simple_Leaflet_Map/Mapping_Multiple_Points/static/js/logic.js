a2f00b3ef4a9b1718169f12ea582a04f7c8db2fa
shawnkitagawa <seankitagawa@gmail.com>
2022-12-19 22:49:50 -0800

    update

--- Simple_Leaflet_Map/Mapping_Multiple_Points/static/js/logic.js
+++ Simple_Leaflet_Map/Mapping_Multiple_Points/static/js/logic.js
@@ -2,18 +2,33 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+let map = L.map("mapid").setView([34.0522, -118.2437], 4);
 
-// Add a marker to the map for Los Angeles, Calfornia.
+  // Loop throught the cities array and create one marker for each city.
 
-// let marker = L.marker([34.0522,-118.2437]).addTo(map);
 
-L.circleMarker([34.0522, -118.2437],
-    {
-        radius: 300,
-        color: 'black',
-        fillColor: 'yellow'
-    }).addTo(map);
+  // Get data from cities.js
+
+
+let cityData = cities;
+
+cityData.forEach(city => {
+    
+    console.log(city);
+
+
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: 'yellow'
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(map)
+
+});
+
+
+
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
